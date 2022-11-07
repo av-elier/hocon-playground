98fa0eaf5188fd7536839182a8f900c0e243811d
Alexey Pozdnyakov <apozdnyakov@tradingview.com>
2022-11-07 23:55:09 +0300

    resolve with no system envs

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -14,6 +14,8 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index())
   }
 
+  val confResolverOptions = ConfigResolveOptions.noSystem()
+
   def hoconToJson() = Action(parse.tolerantText) { implicit request =>
     val resolve = request.queryString.getOrElse("resolve", Seq()).headOption.map(_.toBoolean).getOrElse(false);
     val json = request.queryString.getOrElse("json", Seq()).headOption.map(_.toBoolean).getOrElse(false);
@@ -22,20 +24,16 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     val originComments = request.queryString.getOrElse("originComments", Seq()).headOption.map(_.toBoolean).getOrElse(false);
     val parseOptions = ConfigParseOptions.defaults().setIncluder(new ProhibitIncluder());
     val conf = Try(ConfigFactory.parseString(request.body, parseOptions))
+
     conf match {
       case Success(conf) => {
-        val resolvedConf = if (resolve) conf.resolve() else conf
+        val resolvedConf = if (resolve) conf.resolve(confResolverOptions) else conf
         val renderConfig = ConfigRenderOptions.defaults()
           .setJson(json)
           .setFormatted(formatted)
           .setComments(comments)
           .setOriginComments(originComments);
         val rendered = resolvedConf.root().render(renderConfig)
-        val dbUrl = System.getenv("DATABASE_URL")
-        val secret = System.getenv("APPLICATION_SECRET")
-        if ((dbUrl != null && rendered.contains(dbUrl)) || (secret != null && rendered.contains(secret))) {
-          throw new Exception("you doing somethind nasty with APPLICATION_SECRET, right?")
-        }
         Ok(rendered)
       }
       case Failure(err) => Ok(err.getMessage)
